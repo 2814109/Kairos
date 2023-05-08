d0428b49142b42f6d4ca7c2659688e255e681058
2814109 <2814109k@gmail.com>
2023-05-08 21:36:25 +0900

    Initial Commit

---
+++ Kairos/KairosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairosApp.swift
+//  Kairos
+//
+//  Created by kyosuke on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct KairosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
