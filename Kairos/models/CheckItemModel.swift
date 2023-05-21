8f369e2f9ef47e10b8459c3372ec8a1a7a92a787
2814109 <2814109k@gmail.com>
2023-05-22 05:50:22 +0900

    create Model

---
+++ Kairos/models/CheckItemModel.swift
@@ -0,0 +1,31 @@
+//
+//  CheckItem.swift
+//  Kairos
+//
+//  Created by kyosuke on 2023/05/21.
+//
+
+import Foundation
+import RealmSwift
+
+class CheckItemModel: Object {
+    
+    @objc dynamic var taskName: String = ""
+    @objc dynamic var isChecked: Bool = false
+    @objc dynamic var createdAt = Date()
+
+    @objc dynamic var categoryModel: CategoryModel?
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
+
+class CategoryModel: Object {
+    @objc dynamic var categoryName: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
